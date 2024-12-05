ff6684b993f51d0096a010a4b324e18c0ca39ca7
josefchann <hfyjc17@nottingham.edu.my>
2024-12-06 00:27:11 +0800

    added: better commenting conventions

--- app/src/main/java/com/haw/mobiledeviceprogramming/presentation/screen/ScheduleScreen.kt
+++ app/src/main/java/com/haw/mobiledeviceprogramming/presentation/screen/ScheduleScreen.kt
@@ -1,19 +1,14 @@
 package com.haw.mobiledeviceprogramming.presentation.screen
 
 import DoctorViewModel
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -24,7 +19,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.haw.mobiledeviceprogramming.presentation.components.ScheduleDoctorCard
